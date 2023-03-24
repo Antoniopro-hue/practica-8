e6712bfcb1e937846a5b4d3927e7da711db70dd3
Antoniopro-hue <63246009+Antoniopro-hue@users.noreply.github.com>
2023-03-24 12:12:09 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 let brillo = 0
 while (pins.digitalReadPin(DigitalPin.P10) == 0) {
     pins.digitalWritePin(DigitalPin.P0, 0)
+    brillo = 0
 }
-while (brillo == 500) {
-    brillo += 1
+while (brillo == 800) {
+    brillo += 100
     pins.analogWritePin(AnalogPin.P0, brillo)
 }
 brillo = 0
